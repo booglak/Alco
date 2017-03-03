35bb40e3d66a1015088b51b0310da843954ecf39
I_Buglak <i.buglak@nullgr.com>
2017-03-03 12:52:02 +0200

    rename reses NoActionBar

--- app/src/main/java/com/example/igor/alco/StartActivity.java
+++ app/src/main/java/com/example/igor/alco/StartActivity.java
@@ -1,7 +1,10 @@
 package com.example.igor.alco;
 
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class StartActivity extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+
     }
 }
