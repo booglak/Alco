083453b1407e135b4218828692446c14c489e183
I_Buglak <i.buglak@nullgr.com>
2017-03-03 14:45:52 +0200

    GameActivity UI

--- app/src/main/java/com/example/igor/alco/StartActivity.java
+++ app/src/main/java/com/example/igor/alco/StartActivity.java
@@ -1,12 +1,18 @@
 package com.example.igor.alco;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.TextView;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private TextView tvStartApp;
+    private TextView tvExitApp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,27 @@ public class StartActivity extends AppCompatActivity {
 
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
+        tvStartApp = (TextView)findViewById(R.id.tvStartApp);
+        tvExitApp = (TextView) findViewById(R.id.tvExitApp);
 
+        tvStartApp.setOnClickListener(this);
+        tvExitApp.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.tvStartApp:
+                Intent intentStart = new Intent(this, GameActivity.class);
+                startActivity(intentStart);
+                break;
+            case R.id.tvExitApp:
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
